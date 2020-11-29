9f74b8eea3eeccf7b00a38fb8f714eae87c2bbb9
CantPauseMom <sebciol191@gmail.com>
2020-11-29 14:35:27 +0100

    Make def's in a single class, get heartbeat rate from discord API result

--- bot.py
+++ bot.py
@@ -35,38 +35,53 @@ reddit = praw.Reddit(
      user_agent=U_Agent
      )
 
+class Connection():
+    def get_token(self):
 
-def get_token():
+        data = {
+            'grant_type': 'client_credentials',
+            'scope': 'bot'
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        r = requests.post('%s/oauth2/token' % API_ENDPOINT, data=data, headers=headers, auth=(client, sec))
+        r.raise_for_status()
+        return r.json()
 
-    data = {
-        'grant_type': 'client_credentials',
-        'scope': 'bot'
-    }
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    r = requests.post('%s/oauth2/token' % API_ENDPOINT, data=data, headers=headers, auth=(client, sec))
-    r.raise_for_status()
-    return r.json()
+    def get_gateway(self):
+        request = requests.get('%s/gateway' % API_ENDPOINT)
+        request.raise_for_status()
+        return request.json()
+
+    def get_info(self):
+        uri = 'wss://gateway.discord.gg/?v=8&encoding=json'
+        conn = create_connection(uri)
+        global result
+        result = conn.recv()
+
+        J_File = json.dumps(result)
+        test = json.loads(J_File)
+
+        return result
+
+    def get_heartbeatrate(self):
+        heartbeat_rate = result[53:58]
+        return heartbeat_rate
+
+    def connect(self):
+        uri = 'wss://gateway.discord.gg/?v=8&encoding=json'
+
+        send_opcode1 ={
+        "op": 1,
+        "d": None
+        }
 
 
-def get_gateway():
-    request = requests.get('%s/gateway' % API_ENDPOINT)
-    request.raise_for_status()
-    return request.json()
-
-
-def get_info():
-    uri = 'wss://gateway.discord.gg/?v=8&encoding=json'
-    conn = create_connection(uri)
-    result = conn.recv()
-    conn.close()
-    return result
-
-
-print(get_gateway())
-print(get_token())
-print(get_info())
+    print(get_gateway())
+    print(get_token())
+    print(get_info())
+    print(get_heartbeatrate())
 while True:
     print('got into loop')
     time.sleep(20)
