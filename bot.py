b53ee7d9718196f11e7670d3eff76a7a1619d942
CantPauseMom <sebciol191@gmail.com>
2020-12-01 19:21:23 +0100

    change code into more pleasant to eyes version, use classes

--- bot.py
+++ bot.py
@@ -3,29 +3,27 @@ import os
 import praw
 import discord
 import time
+from gevent import monkey
 import gevent
-import random
 import requests
 import json
 from websocket import create_connection
 from dotenv import load_dotenv
 from imgurpython import ImgurClient
-
+import aiohttp
 
 # LOAD .env FILE
 load_dotenv()
 
 # RETRIEVE SECRETS FROM .env
-TOKEN = os.getenv('DISCORD_TOKEN')
+
 client = os.getenv('client_id')
 sec = os.getenv('my_client_secret')
 U_Agent = os.getenv('user_agent')
 ImgurID = os.getenv('IMGUR_ID')
 ImgurSecret = os.getenv('IMGUR_SECRET')
-API_ENDPOINT = 'https://discord.com/api/v8'
+#API_ENDPOINT = 'https://discord.com/api/v8'
 uri = 'wss://gateway.discord.gg/?v=8&encoding=json'
-# CREATE DISCORD CLIENT INSTANCE
-D_Client = discord.Client()
 
 # LOAD IMGUR CLIENT
 imgur = ImgurClient(ImgurID, ImgurSecret)
@@ -38,7 +36,11 @@ reddit = praw.Reddit(
      )
 
 
-def get_token():
+class Data:
+    API_ENDPOINT = 'https://discord.com/api/v8'
+
+    uri = "wss://gateway.discord.gg/"
+    TOKEN = os.getenv('DISCORD_TOKEN')
 
     data = {
         'grant_type': 'client_credentials',
@@ -47,29 +49,10 @@ def get_token():
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded'
     }
-    r = requests.post('%s/oauth2/token' % API_ENDPOINT, data=data, headers=headers, auth=(client, sec))
-    r.raise_for_status()
-    return r.json()
-
-
-async def establish_connection():
-    conn = create_connection(uri)
-    result = conn.recv()
-    heartbeat_rate = result[53:58]
-    print(heartbeat_rate)
     opcode1 = {
         "op": 1,
         "d": None
     }
-
-    async def send_opcode1():
-        loop = asyncio.new_event_loop()
-        json_pack = json.dumps(opcode1)
-        await conn.send(json_pack)
-        gevent.sleep(int(heartbeat_rate) / 1000)
-        print("package sent")
-        loop.run_forever()
-
     pack = {
         "op": 2,
         "d": {
@@ -82,12 +65,68 @@ async def establish_connection():
             }
         }
     }
-    pack_json = json.dumps(pack)
-    conn.send(pack_json)
-    print("pack sent")
 
-print(get_token())
-asyncio.run(establish_connection())
 
-while True:
-    time.sleep(5)
+class HTTP(Data):
+
+    def get_gateway(self, API_ENDPOINT):
+        response = requests.get(f'{API_ENDPOINT}/gateway').json()
+        data = response['url']
+        print(data)
+        return data
+
+    def get_bot_gateway(self, API_ENDPOINT):
+        response = requests.get(f'{API_ENDPOINT}/gateway').json()
+        data = response['url']
+        return data
+
+    def get_heartbeat(self, data,):
+        conn = create_connection(data)
+        result = conn.recv()
+        result_json = json.loads(result)
+        json_slice = result_json['d']
+        heartbeat = json_slice['heartbeat_interval']
+        print(heartbeat)
+        return heartbeat
+
+
+class Client:
+    def __init__(self):
+        super(Client, self).__init__()
+        self.http = HTTP
+
+
+# async def connection():
+#     async def establish_connection():
+#
+#         conn = create_connection(uri)
+#         result = conn.recv()
+#         heartbeat_rate = result[53:58]
+#         print(heartbeat_rate)
+#
+#         pack_json = json.dumps(pack)
+#         conn.send(pack_json)
+#         print("pack sent")
+#         return opcode1, conn, heartbeat_rate
+#
+#     loop = asyncio.get_event_loop()
+#
+#     async def send_opcode1(opcode1, conn, heartbeat_rate):
+#         while True:
+#             asyncio.new_event_loop()
+#             json_pack = json.dumps(opcode1)
+#             conn.send(json_pack)
+#             print(f"pack sent before sleep. Rate : {heartbeat_rate}")
+#             await gevent.sleep(int(heartbeat_rate) / 1000)
+#             print("package sent")
+#
+#     loop.run_forever()
+#
+# connection()
+HTTP.get_gateway(HTTP, API_ENDPOINT=Data.API_ENDPOINT)
+HTTP.get_bot_gateway(HTTP, API_ENDPOINT=Data.API_ENDPOINT)
+HTTP.get_heartbeat(HTTP,HTTP.get_gateway(HTTP, API_ENDPOINT=Data.API_ENDPOINT))
+if __name__ == '__main__':
+    while True:
+        print('end')
+        time.sleep(5)
