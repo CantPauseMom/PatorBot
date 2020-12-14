4186fc80bd57a573b653fe7ddb6663bc7a85ad80
CantPauseMom <sebciol191@gmail.com>
2020-12-14 17:23:31 +0100

    make function use separate thread

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
+import asyncio
+
 from gevent import monkey
+
 monkey.patch_all()
+import threading
 import json
 import os
 import time
@@ -8,6 +12,7 @@ import requests
 from websocket import create_connection
 from dotenv import load_dotenv
 from data import Data
+
 load_dotenv()
 
 client = os.getenv('client_id')
@@ -34,6 +39,18 @@ def connection():
         print('Heartbeat =', heartbeat)
         return heartbeat
 
+    def send_heartbeat():
+        while True:
+            print("sending heartbeat")
+            packet = json.dumps(Data.opcode1)
+            conn.send(packet)
+            gevent.sleep(41250 / 1000)
+
+    # send_heartbeat(heartbeat=get_heartbeat())
+    thread = threading.Thread(target=send_heartbeat)
+    thread.daemon = True
+    thread.start()
+
     def send_token():
         packet = json.dumps(Data.pack)
         conn.send(packet)
@@ -41,23 +58,20 @@ def connection():
 
     send_token()
 
-    def send_heartbeat(heartbeat):
-        while True:
-            print("sending heartbeat")
-            packet = json.dumps(Data.opcode1)
-            conn.send(packet)
-            gevent.sleep(heartbeat / 1000)
-            print("heartbeat sent")
-
     def get_ready():
-        ready = conn.recv()
+        time.sleep(1)
+        readymsg = conn.recv()
         print("in on_ready func")
-        print(str(ready))
+        print(str(readymsg))
 
-    gevent.joinall([
-        gevent.spawn(send_heartbeat(heartbeat=get_heartbeat())),
-        gevent.spawn(get_ready())
-        ])
+    tr = threading.Thread(target=get_ready)
+    tr.daemon = True
+    tr.start()
+
+
+def somefunc():
+    print('someloop')
+    time.sleep(2)
 
 
 connection()
